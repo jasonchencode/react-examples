4dfc00a9a75209442ae220ff730c63f12c379772
Jason Chen <jasonchenn67@gmail.com>
2026-09-01 23:31:50 -0300

    create counter that only updates when backgyround is changed

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -5,28 +5,38 @@ const COLORS = ['pink', 'green', 'blue', 'yellow', 'purple'];
 
 function App() {
   const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
-
-  const onButtonClick = (color) => () => {
+  const [x, setX] = useState(0);
+  
+  const onButtonClick = (color, x) => () => {
+    const initialBackground = backgroundColor;
     setBackgroundColor(color);
+    if (!(initialBackground === color)) {
+      setX(x + 1); 
+    }
+
   };
 
   return (
     <div
       className="App"
       style={{
-        backgroundColor,
+        backgroundColor
       }}
     >
       {COLORS.map((color) => (
         <button
           type="button"
           key={color}
-          onClick={onButtonClick(color)}
+          onClick={onButtonClick(color, x)}
           className={backgroundColor === color ? 'selected' : ''}
         >
           {color}
         </button>
+      
       ))}
+      <button>
+        {x}
+      </button>
     </div>
   );
 }
